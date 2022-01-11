1e6f78df9687beb7b42ebbfe29d32358f575c97c
KASKUS <>
2022-01-11 14:53:58 +0000

    Update onReady message

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
-const config = require('./config/settings');
+const settings = require('./config/settings');
 
-app.listen(config.port, () => {
-	console.log('Test Server Listening On ')
+app.listen(settings.port, () => {
+	console.log('Test Server Is Ready')
 });
