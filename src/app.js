2c58f340f0584001d595b9910cab006f7856164a
KASKUS <>
2022-01-11 13:58:38 +0000

    Update middlewares and add some useful modules

--- src/app.js
+++ src/app.js
@@ -3,9 +3,12 @@ global.setting = require('./config/settings');
 
 // Express modules
 const express = require('express');
+const compress = require('compression')
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const xss = require('xss-clean')
 const cors = require('cors');
 
 // Middlewares
@@ -15,17 +18,22 @@ const { notFoundHandler, errorHandler } = require('./middleware/errorHandler')
 const app = express();
 
 // App uses
-app.use(helmet());
-app.use(cookieParser())
-app.use(bodyParser.json());
+app.use(compress()) // gzip compression
+app.use(methodOverride()) // lets you use HTTP verbs
+app.use(helmet()); // secure apps by setting various HTTP headers
+app.use(morgan('dev')); // Simple logger
+app.use(cors()) // enable cors
+app.use(xss()) // handler xss attack
+app.use(cookieParser()); // cookie parser for cookie management
+app.use(bodyParser.json({ limit: '200kb' }));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Use router
 app.use('/', require('./routes/index'));
 
 // Use middlware
-app.use(notFoundHandler)
-app.use(errorHandler)
+app.use(notFoundHandler) // 404 request handler
+app.use(errorHandler) // 500 internal error request handler
 
 // Export app
 module.exports = app;
